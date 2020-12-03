521021eb0b695ac6f8fd6a2b1d07bfec4acee13a
María José Marra <majomarra@gmail.com>
2020-12-03 15:08:16 -0300

    Update Tweet.jsx

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -33,6 +33,7 @@ export default function Tweet({ tweet }) {
 			.get(URL, {
 				headers: {
 					Authorization: "Bearer " + loggedUser.accessToken,
+					"Access-Control-Allow-Origin": "*",
 				},
 			})
 			.then((res) => {
